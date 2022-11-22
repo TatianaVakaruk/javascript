aacc48b8154021ec2faa8297fda73c30370c7a1f
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2022-11-22 20:29:02 +0200

    commit1

---
+++ lesson21/task2/index.js
@@ -0,0 +1,10 @@
+function getTitleElement() {
+  const titleElem = document.querySelector(".title");
+  console.dir(titleElem);
+  return titleElem;
+}
+function getInputElement() {
+  const inputElem = document.querySelector("[type = text]");
+  console.dir(inputElem);
+  return inputElem;
+}
