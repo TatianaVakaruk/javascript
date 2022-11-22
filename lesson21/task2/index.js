50ce95f27e2cf4be1f0875f5c4e6746667e5ceca
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2022-11-22 20:48:07 +0200

    commit1

--- lesson21/task2/index.js
+++ lesson21/task2/index.js
@@ -1,10 +1,12 @@
 function getTitleElement() {
-  const titleElem = document.querySelector(".title");
-  console.dir(titleElem);
-  return titleElem;
+  //const titleElem = document.querySelector(".title");
+  console.dir(document.querySelector(".title"));
+  return document.querySelector(".title");
 }
+//console.log(getTitleElement());
 function getInputElement() {
-  const inputElem = document.querySelector("[type = text]");
-  console.dir(inputElem);
-  return inputElem;
+  //const inputElem = document.querySelector("input[type = text]");
+  console.dir(document.querySelector("input[type = text]"));
+  return document.querySelector("input[type = text]");
 }
+//console.log(getInputElement());
