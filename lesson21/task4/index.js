68e90484224304543f3046bdccc126cd92bdee01
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2022-11-23 02:34:22 +0200

    commit1

---
+++ lesson21/task4/index.js
@@ -0,0 +1,23 @@
+export function getTitle() {
+  const titleText = document.querySelector(".title");
+  const text = titleText.textContent;
+  return text;
+}
+
+export function getDescription() {
+  const descriptionText = document.querySelector(".about");
+  const text = descriptionText.innerHTML;
+  return text;
+}
+
+export function getPlans() {
+  const plansText = document.querySelector(".plans");
+  const text = plansText.innerHTML;
+  return text;
+}
+
+export function getGoal() {
+  const goalText = document.querySelector(".goal");
+  const text = goalText.outerHTML;
+  return text;
+}
