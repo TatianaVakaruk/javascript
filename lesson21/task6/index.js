00e1b70236eeaab44a0f62f20d0da3a8d8ab313d
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2022-11-23 03:25:44 +0200

    commit1

--- lesson21/task6/index.js
+++ lesson21/task6/index.js
@@ -1,8 +1,5 @@
-export function setButton(buttonText) {
-  buttonText = "text button";
+function setButton(buttonText) {
   const button1 = document.querySelector("body");
   button1.innerHTML = "<button>" + buttonText + "</button>";
-
   return button1;
 }
-console.log(setButton("text button"));
