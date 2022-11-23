bf54c10dcd50973d5d6fb1b41996da4525969914
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2022-11-23 10:36:45 +0200

    commit1

---
+++ lesson21/task9/index.js
@@ -0,0 +1,15 @@
+export function finishList() {
+  let li = document.createElement("li");
+  li.textContent = "8";
+  document.querySelector("ul").append(li);
+  let li1 = document.createElement("li");
+  li1.textContent = "1";
+  document.querySelector("ul").prepend(li1);
+  let li2 = document.createElement("li");
+  li2.textContent = "4";
+  document.querySelector(".special").before(li2);
+  let li3 = document.createElement("li");
+  li3.textContent = "6";
+  document.querySelector(".special").after(li3);
+}
+console.log(finishList());
