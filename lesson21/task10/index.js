65a1b5cfad54eb052cbec5a3d954fe5baa99ca0b
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2022-11-23 11:23:44 +0200

    commit1

--- lesson21/task10/index.js
+++ lesson21/task10/index.js
@@ -2,6 +2,6 @@ export function finishForm() {
   let input = document.createElement("input");
   input.setAttribute("name", "login");
   input.setAttribute("type", "password");
-  input.querySelector(".login-form").prepend(input);
+  document.querySelector(".login-form").prepend(input);
   return input;
 }
