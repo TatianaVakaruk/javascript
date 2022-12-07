b53262135bff19d33db1abf0b545d98e90b80875
Tatiana Vakaryuk <110806913+TatianaVakaruk@users.noreply.github.com>
2022-12-07 11:32:28 +0200

    Update index.js

--- lesson27/task33/src/scripts/index.js
+++ lesson27/task33/src/scripts/index.js
@@ -1,7 +1,7 @@
 import { initTodoListHandlers } from "./todoList.js";
 import { renderTasks } from "./render.js";
 document.addEventListener("DOMContentLoaded", () => {
-  renderTask();
+  renderTasks();
   initTodoListHandlers();
 });
 const onStorageChange = (e) => {
