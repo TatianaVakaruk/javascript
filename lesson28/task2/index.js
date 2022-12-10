2cdf157caf775895afec68e744c6aa65e0a842c4
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2022-12-10 07:20:20 +0200

    commit1

--- lesson28/task2/index.js
+++ lesson28/task2/index.js
@@ -22,7 +22,7 @@ const tree = {
   ],
 };
 
-const markFavorites = (tree, favorites) => {
+export const markFavorites = (tree, favorites) => {
   const isFavorite = favorites.includes(tree.id);
   return {
     ...tree,
