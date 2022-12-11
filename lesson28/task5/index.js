62a7067abc45a9446ecda5c037f6b238fa5b7940
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2022-12-11 12:48:17 +0200

    commit1

--- lesson28/task5/index.js
+++ lesson28/task5/index.js
@@ -4,34 +4,36 @@ const shmoment = (initValue) => {
     add(arg, value) {
       switch (arg) {
         case "years":
-          let value1 = new Date(value).getMilliseconds();
+          let value1 = value * 1000 * 60 * 60 * 24 * 30 * 12;
           result = result + value1;
           return this;
 
         case "months":
-          let month1 = new Date(value).getMilliseconds();
+          let month1 = value * 1000 * 60 * 60 * 24 * 30;
 
           result = result + month1;
           return this;
-
+        case "days":
+          let day = value * 1000 * 60 * 60 * 24;
+          result = result + day;
+          return this;
         case "hour":
-          let hour1 = new Date(value).getMilliseconds();
+          let hour1 = value * 1000 * 60 * 60;
           result = result + hour1;
           return this;
 
         case "minutes":
-          let minutes1 = new Date(value).getMilliseconds();
+          let minutes1 = value * 1000 * 60;
           result = result + minutes1;
           return this;
 
         case "seconds":
-          let seconds1 = new Date(value).getMilliseconds();
+          let seconds1 = value * 1000;
           result = result + seconds1;
           return this;
 
         case "milliseconds":
-          let milliseconds1 = new Date(value).getMilliseconds();
-          let result = result + milliseconds1;
+          let result = result + value;
           return this;
       }
     },
@@ -39,33 +41,35 @@ const shmoment = (initValue) => {
     subtract(arg, value) {
       switch (arg) {
         case "years":
-          let year1 = new Data(value).getMilliseconds();
+          let year1 = value * 1000 * 60 * 60 * 24 * 30 * 12;
           result = result - year1;
           return this;
 
         case "months":
-          let month1 = new Date(value).getMilliseconds();
+          let month1 = value * 1000 * 60 * 60 * 24 * 30;
           result = result - month1;
           return this;
-
+        case "days":
+          let day = value * 1000 * 60 * 60 * 24;
+          result = result - day;
+          return this;
         case "hour":
-          let hour1 = new Date(value).getMilliseconds();
+          let hour1 = value * 1000 * 60 * 60;
           result = result - hour1;
           return this;
 
         case "minutes":
-          let minutes1 = new Date(value).getMilliseconds();
+          let minutes1 = value * 1000 * 60;
           result = result - minutes1;
           return this;
 
         case "seconds":
-          let seconds1 = new Date(value).getMilliseconds();
-          result = result - seconds1;
+          let seconds1 = value * 1000;
+          result = result + seconds1;
           return this;
 
         case "milliseconds":
-          let milliseconds1 = new Date(value).getMilliseconds();
-          result = result - milliseconds1;
+          result = result - value;
           return this;
       }
     },
