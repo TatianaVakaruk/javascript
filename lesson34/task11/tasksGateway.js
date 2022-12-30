433a3eeb408e4e2f479455ade2a8bae71c04bcdd
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2022-12-30 06:23:59 +0200

    commit1

--- lesson34/task11/tasksGateway.js
+++ lesson34/task11/tasksGateway.js
@@ -1,6 +1,6 @@
 const baseUrl = 'https://crudcrud.com/api/06c54258206b41e5ada127d0fd2d724a/tasks';
 
-const mapTasks = (({_id, ...rest})=>tasks.map(task => ({...rest, id:_id})));
+const mapTasks = (({_id, ...rest})=>tasks.map(rest => ({...rest, id:_id})));
 
 export const getTasksList = ()=>{
     return fetch(baseUrl).then(response=>response.json()).then(tasks=>tasks.map(task => mapTasks(task)));
