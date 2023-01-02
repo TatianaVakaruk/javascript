8f0140fcd7a2a77d4394984eaa2b3500c4077a3e
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2023-01-02 21:41:59 +0200

    commit1

--- lesson35/task3/gateways.js
+++ lesson35/task3/gateways.js
@@ -7,7 +7,7 @@ export const fetchUserData = userName =>{
 });
 };
   export const fetchRepositories=url =>{
-  fetch(url).then(response=>{
+ return fetch(url).then(response=>{
     if (response.ok){
         return response.json()
     }
--- lesson35/task3/repos.js
+++ lesson35/task3/repos.js
@@ -14,5 +14,5 @@ export const renderRepos = reposList=>{
 
     })
    cleanReposList();
-    listElem.append(...reposListElems)
+    listElem.append(...reposListElems);
 }
