052880c1cc485c7163a8a97b87fe23fdb5912bf9
Tatiana Vakaryuk <tatiana.vakaruk1@gmail.com>
2023-01-02 18:45:42 +0200

    commit1

---
+++ lesson35/task3/gateways.js
@@ -0,0 +1,16 @@
+export const fetchUserData = userName =>{
+    return fetch(`https://api.github.com/users/${userName}`).then(response=>{
+    if (response.ok){
+        return response.json()
+    }
+    throw new Error('Faild to load data');
+});
+};
+  export const fetchRepositories=url =>{
+  fetch(url).then(response=>{
+    if (response.ok){
+        return response.json()
+    }
+    throw new Error('Faild to load data');
+});
+};
